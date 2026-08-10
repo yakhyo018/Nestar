e7c01fea5ea78be03e217d4394b4745abf52c364
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-08-10 17:05:39 +0900

    feat: build project standarts

--- apps/nestar-api/src/app.module.ts
+++ apps/nestar-api/src/app.module.ts
@@ -5,11 +5,20 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
-		GraphQLModule.forRoot({ driver: ApolloDriver, playground: true, uploads: false, autoSchemaFile: true }),
+		GraphQLModule.forRoot({
+			driver: ApolloDriver,
+			playground: true,
+			uploads: false,
+			autoSchemaFile: true,
+		}),
+		ComponentsModule,
+		DatabaseModule,
 	],
 	controllers: [AppController],
 	providers: [AppService, AppResolver],
---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,21 @@
+export enum Messages {
+	SOMETHING_WENT_WRONG = 'Something went wrong!',
+	NO_DATA_FOUND = 'No data is found!',
+	CREATE_FAILED = 'Create is failed!',
+	UPDATE_FAILED = 'Update is failed!',
+	REMOVE_FAILED = 'Remove failed!',
+	UPLOAD_FAILED = 'Upload failed!',
+	BAD_REQUEST = 'Bad Request',
+
+	NO_MEMBER_NICK = 'no member with that member nick!',
+	BLOCKED_USER = 'You have been blocked, please contact admin!',
+	WRONG_PASSWORD = 'wrong password entered, please try again',
+	USED_NICK_PHONE = 'you are inserting already used nick or phone!',
+	NOT_AUTHENTICATED = 'You are not authenticated, login first!',
+	TOKEN_CREATION_FAILED = 'Token creation error!',
+	TOKEN_NOT_EXIST = 'Bearer Token is not provided!',
+	ONLY_SPECIFIC_ROLES_ALLOWED = 'Allowed only for members with specific roles!',
+	NOT_ALLOWED_REQUEST = 'Not Allowed Request!',
+	PROVIDE_ALLOWED_FORMAT = 'Please provide jpg, jpeg, or png images!',
+	SELF_SUBSCRIPTION_DENIED = 'Self subscription is denied!',
+}
