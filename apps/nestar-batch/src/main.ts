851c7c98dd621d2c22d830b030b7ce9845a1af35
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-08-10 16:33:53 +0900

    feat: integration GraphQLModule and build first GraphQL api

--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestarBatchModule } from './nestar-batch.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NestarBatchModule);
-  await app.listen(process.env.PORT_BATCH ?? 3001);
+	const app = await NestFactory.create(NestarBatchModule);
+	await app.listen(process.env.PORT_BATCH ?? 3001);
 }
 bootstrap();
