3a5e01c060ea1eb0fbcbb545d9b0a6b4f54defef
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-08-14 14:53:49 +0900

    feat: create member schema model

--- apps/nestar-api/src/database/database.module.ts
+++ apps/nestar-api/src/database/database.module.ts
@@ -16,7 +16,7 @@ export class DatabaseModule {
 	constructor(@InjectConnection() private readonly connection: Connection) {
 		if (connection.readyState === 1) {
 			console.log(
-				`MongoDB is connecting into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'}`,
+				`MongoDB is connecting into ${process.env.NODE_ENV === 'production' ? 'production' : 'development'} DB`,
 			);
 		} else {
 			console.log('DB is not connected!');
