6994a951e6ca78496fce4ca5ec057a2a099e2c2e
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-08-21 18:13:02 +0900

    feat: create etAllMembersByAdmin and updateMemberByAdmin GraphQL APIs

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -32,4 +32,19 @@ export class MemberResolver {
 		console.log('Query getMember');
 		return this.memberService.getMember();
 	}
+
+	/** ADMIN **/
+
+	// Authorization: ADMIN
+	@Mutation(() => String)
+	public async getAllMembersByAdmin(): Promise<string> {
+		return this.memberService.getAllMemberByAdmin();
+	}
+
+	// Authorization: ADMIN
+	@Mutation(() => String)
+	public async updateMemberByAdmin(): Promise<string> {
+		console.log('Mutation: updateMemberByAdmin');
+		return this.memberService.updateMemberByAdmin();
+	}
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -42,7 +42,6 @@ export class MemberService {
 			throw new InternalServerErrorException(Messages.BLOCKED_USER);
 		}
 
-		//TODO: Compare passwords
 		const isMatch = await this.authService.comparePassword(
 			input.memberPassword,
 			response.memberPassword!,
@@ -59,5 +58,13 @@ export class MemberService {
 
 	public async getMember(): Promise<string> {
 		return 'getMember executed';
+	}
+
+	public async getAllMemberByAdmin(): Promise<string> {
+		return 'getAllMemberByAdmin';
+	}
+
+	public async updateMemberByAdmin(): Promise<string> {
+		return 'updateMemberByAdmin';
 	}
 }
