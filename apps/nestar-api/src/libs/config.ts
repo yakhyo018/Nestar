5a6ed8c95821d4baec05a456c8735794fe120af3
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-08-28 17:02:03 +0900

    feat: develop single and multi image uploader mutation GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -10,6 +10,16 @@ export const availableAgentSorts = [
 
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
 
+/* IMAGE CONFIGURATION */
+import { v4 as uuidv4 } from 'uuid';
+import * as path from 'path';
+
+export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+export const getSerialForImage = (filename: string) => {
+	const ext = path.parse(filename).ext;
+	return uuidv4() + ext;
+};
+
 export const shapeIntoMongoObjectId = (target: any) => {
 	return typeof target === 'string' ? new ObjectId(target) : target;
 };
