187e3ff4e65a4e5a274e53c3d5385aad37b7012d
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-09-02 18:00:38 +0900

    feat: develop removeCommentByAdmin GraphQL api

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -9,6 +9,9 @@ import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { Comment, Comments } from '../../libs/dto/comment/comment';
+import { MemberType } from '../../libs/enums/member.enum';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
 export class CommentResolver {
@@ -44,5 +47,16 @@ export class CommentResolver {
 		console.log('Query: getComments');
 		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
 		return await this.commentService.getComments(memberId, input);
+	}
+
+	/** ADMIN **/
+
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation((returns) => Comment)
+	public async removeCommentByAdmin(@Args('commentId') input: string): Promise<Comment> {
+		console.log('Mutation: removeCommentByAdmin');
+		const commentId = shapeIntoMongoObjectId(input);
+		return await this.commentService.removeCommentByAdmin(commentId);
 	}
 }
--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -102,4 +102,10 @@ export class CommentService {
 
 		return result[0];
 	}
+
+	public async removeCommentByAdmin(input: ObjectId): Promise<Comment> {
+		const result = await this.commentModel.findByIdAndDelete(input);
+		if (!result) throw new InternalServerErrorException(Messages.REMOVE_FAILED);
+		return result;
+	}
 }
