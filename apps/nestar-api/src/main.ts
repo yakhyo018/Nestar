bc04e134b3c4a8641f59c5b1278c1bc1a689d89e
Antony(Yahyo) <choriyevyahyo@gmail.com>
2026-09-09 17:19:21 +0900

    feat: create WebSocket process

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 import { graphqlUploadExpress } from 'graphql-upload';
 import * as express from 'express';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
 	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
 	app.use('/uploads', express.static('./uploads'));
 
+	app.useWebSocketAdapter(new WsAdapter(app));
 	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
